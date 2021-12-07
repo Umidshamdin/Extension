066a32d2ab9956da94f5b8194a0382f609775204
[umid shamdinli] <[umidash@code.edu.az]>
2021-12-07 17:34:17 +0400

    Extension task

---
+++ ConsoleApp1/ExtensionString.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Text.RegularExpressions;
+
+namespace ConsoleApp1
+{
+    public static class ExtensionString
+    {
+
+        public static int Repeat(this string str, string find)
+        {
+            return Regex.Matches(str, find).Count;
+
+
+            
+
+            
+        }
+    }
+}
---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            
+          string  str = "Salam Umid. Necesen Umid.";
+
+            int result = str.Repeat("Umid");
+            Console.WriteLine(result);
+        }
+    }
+}
